d93973be38423e48284b6f4e4cee968ca95e7a17
Rahul <rtvconcepts@gmail.com>
2025-08-20 11:25:03 -0400

    added print line

--- B.java
+++ B.java
@@ -1,6 +1,7 @@
 public class B {
-        public void fileB () {}
-        System.out.println("file B");
+        public void fileB () {
+                System.out.println("file B");
+                System.out.println("one more change made to file B");
 }
 
 }

144eef3926d98b0951df721755c0591348c77fc0
Rahul <rtvconcepts@gmail.com>
2025-08-20 13:42:35 -0400

    trying to update A.java

--- A.java
+++ A.java
@@ -1,5 +1,6 @@
 public class A {
     public void newMethod () {
     System.out.println("file A");
+    System.out.println("Fred wuz here");
     }
 }
--- B.java
+++ B.java
@@ -2,6 +2,8 @@ public class B {
         public void fileB () {
                 System.out.println("file B");
                 System.out.println("one more change made to file B");
+                                System.out.println("Burney woz here");
+
 }
 
 }

f80174210fe8e4bbdf85d5af2b3003888264b0f3
Rahul <rtvconcepts@gmail.com>
2025-08-20 11:50:50 -0400

    updated with tags removed

--- Calc.java
+++ Calc.java
@@ -1,10 +1,8 @@
 public class Calc {
     public statc void main (String[] args) {
-<<<<<<< HEAD
+
         System.out.println("Hello, Wonderful suckers");
-=======
         System.out.println("Hello, suckers");
->>>>>>> bugFix192
 
     }
 
